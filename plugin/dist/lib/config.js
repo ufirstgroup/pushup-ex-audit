5fee319d7a377309f4ad42ab1a3813c7572fdea8
GitHub Action <action@github.com>
2024-09-11 06:33:36 +0000

    Build TypeScript [skip ci]

--- plugin/dist/lib/config.js
+++ plugin/dist/lib/config.js
@@ -4,10 +4,10 @@ import { writeFile } from 'fs/promises';
 import { PLUGIN_CONFIG_PATH, RUNNER_OUTPUT_PATH } from './index.js';
 async function createRunnerConfig(runnerScriptPath, options) {
     const { path } = options;
-    await ensureDirectoryExists(dirname(PLUGIN_CONFIG_PATH));
     await ensureDirectoryExists(dirname(RUNNER_OUTPUT_PATH));
-    await writeFile(PLUGIN_CONFIG_PATH, JSON.stringify(path));
+    await ensureDirectoryExists(dirname(PLUGIN_CONFIG_PATH));
     await writeFile(RUNNER_OUTPUT_PATH, JSON.stringify({}));
+    await writeFile(PLUGIN_CONFIG_PATH, JSON.stringify(path));
     return {
         command: `node`,
         args: [filePathToCliArg(runnerScriptPath)],
